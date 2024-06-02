50ce7c1633983d76d638221ea06dbb30c0b967f0
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2024-06-02 17:34:15 +0300

    add index.html and script.js (first push)

---
+++ script.js
@@ -0,0 +1,98 @@
+const arr = ["rock","paper","scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice = "";
+let computerChoice = "";
+
+playRound(humanScore,computerScore,computerChoice);
+
+function getComputerChoice(){
+    let x = arr[Math.floor(Math.random()*3)];
+    return computerChoice=x;
+}
+
+function  getHumanChoice(){
+    let answer = prompt("Please enter your choice (rock, paper, scissors): ");
+    answer = answer.toLowerCase();
+    let x= arr.length
+    let y = 0
+    for(let i=0;i<x;i++){
+        arr_item=arr[i];
+        if(arr_item==answer){
+            y += 1;       
+            if(y==1){
+                return humanChoice=answer;
+            }
+        }
+        else{
+            return humanChoice="wrong";
+        }
+    }
+}
+
+function playRound(humanScore,computerScore){
+    while(humanScore < 5 && computerScore < 5){
+        getHumanChoice();
+        getComputerChoice();
+
+        console.log("Human choice: "+humanChoice);
+        console.log("Computer choice: "+computerChoice);
+
+        if(humanChoice=="wrong"){
+            alert("Pleas type correct word (rock, paper, scissors).");
+        }
+        else{
+            if(humanChoice==computerChoice){
+                alert("No one win this round !"+getScore(humanScore,computerScore,computerChoice));
+            }
+            else{
+                if(humanChoice=="rock"){
+                    if(computerChoice=="paper"){
+                        computerScore += 1;
+                        alert("Computer win this round !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                    else{
+                        humanScore +=1;
+                        alert("You win !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                }
+                else if(humanChoice=="paper"){
+                    if(computerChoice=="rock"){
+                        humanScore += 1;
+                        alert("You win !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                    else{
+                        computerScore += 1;
+                        alert("Computer win this round !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                }
+                else{
+                    if(computerChoice=="rock"){
+                        computerScore += 1;
+                        alert("Computer win this round !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                    else{
+                        humanScore += 1;
+                        alert("You win !"+getScore(humanScore,computerScore,computerChoice));
+                    }
+                }
+        
+            }
+        }     
+        if(humanScore==5){
+            alert("Human win."+getScore(humanScore,computerScore));
+        }
+        if(computerScore==5){
+            alert("Computer win."+getScore(humanScore,computerScore));
+        }
+    }
+}
+
+function getScore(x,y,z){
+    if(z){
+        return "\n\nComputer Choice: "+z+"\nYour Score: "+x+"\nComputer Score: "+y;    
+    }
+    else{
+        return "\n\nYour Score: "+x+"\nComputer Score: "+y;
+    }
+}
